e85992ca6efabd60793885b5f2cb93981e2564ec
Dmitry <dimas66@mail.ru>
2022-10-23 17:44:49 +0500

    Домашнее задание HW2
    - заменен файл checkstyle.xml. В качестве исходного использован файл google_checks.xml, из которого удалены не нужные на данном этапе проверки
    - приведение проекта к checkstyle

--- src/MainArray.java
+++ src/MainArray.java
@@ -1,6 +1,5 @@
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.ArrayStorage;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,7 +9,7 @@ import java.io.InputStreamReader;
  * (just run, no need to understand)
  */
 public class MainArray {
-    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
